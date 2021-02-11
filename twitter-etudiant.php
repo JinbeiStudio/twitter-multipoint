ef7b9858ba53c328fc4762629acff5b120dcb0e0
JinbeiStudio <julien.gabriel@me.com>
2021-02-11 10:55:42 +0000

    api settings

--- twitter-etudiant.php
+++ twitter-etudiant.php
@@ -4,10 +4,10 @@
      
     /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
     $settings = array(
-        'oauth_access_token' => "...",
-        'oauth_access_token_secret' => "...",
-        'consumer_key' => "...",
-        'consumer_secret' => "..."
+        'oauth_access_token' => "2348986722-DIF6jg9iLe2NBFIyANE3h0SkrmoltjmOljJF4Tg",
+        'oauth_access_token_secret' => "kQIsM6kRSGcV5CS2mP1unFMYjvuGpEoho7hsdK5IlTErX",
+        'consumer_key' => "rHNPhJIO1GBNmKfFGqICjcyTY",
+        'consumer_secret' => "dfBP1RDT4KZ9NVEuvqdSgvCzLmombznXU1zz1iTKx3C9T9CpA7"
     );
     
 
@@ -21,6 +21,3 @@ $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setGetfield($getfield)
                  ->buildOauth($url, $requestMethod)
                  ->performRequest();
-
-
-?>
