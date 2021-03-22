399015f1c23a13106dce008cc6ed5ff8e609689b
GabrielTavernier <gabrieltavernier@gmail.com>
2021-03-22 14:07:48 +0000

    maj

--- result.php
+++ result.php
@@ -12,8 +12,9 @@ $tweets = $resultTwitter->statuses; ?>
 <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title"><?php echo $value->user->name;  ?></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h6><?php echo $value->created_at; ?></h6>
+    <p class="card-text"><?php echo $value->text;  ?></p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
   </div>
 </div>
 
