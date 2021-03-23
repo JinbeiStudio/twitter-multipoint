b632e9bd3717e9a72bc1b4e49e900bba4f0312a7
EDurdos <emeric.durdos@gmail.com>
2021-03-23 17:06:36 +0000

    nb de resultat ou message si pas de resultat

--- views/template/result.php
+++ views/template/result.php
@@ -1,6 +1,12 @@
 <?php
-//Buocle d'affichage des cartes de tweets
-foreach (unserialize($_SESSION['recherche']) as $tweet) {
+
+$tweets = unserialize($_SESSION['recherche']);
+
+//Affichage du nb de twweets trouvés ou message en cas de résultat nul
+echo count($tweets) ? count($tweets).' Résultats' :  'Aucun résultat n\'as été trouvé. Elargissez votre recherche.';
+
+//Boucle d'affichage des cartes de tweets
+foreach ($tweets as $tweet) {
     echo $tweet->display();
 } 
 ?>
