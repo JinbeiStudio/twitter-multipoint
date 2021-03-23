e95deab2148c00034307cf6f084e8df7af9efe01
GabrielTavernier <gabrieltavernier@gmail.com>
2021-03-23 17:56:39 +0000

    maj result

--- views/template/result.php
+++ views/template/result.php
@@ -1,12 +1,39 @@
 <!--p>Result</p-->
 
+<pre>
 <?php
+// print_r(unserialize($_SESSION['recherche'])); 
+?>
+</pre>
 
-/*echo '<pre>';
-print_r(unserialize($_SESSION['recherche']));
-echo '</pre>';*/
 
-foreach (unserialize($_SESSION['recherche']) as $tweet) {
-    // echo 'test';
-    echo $tweet->display();
-} ?>
+<section class="row">
+
+    <?php
+    foreach (unserialize($_SESSION['recherche']) as $tweet) {
+        // formattage des variables
+        $date = new DateTime($tweet->created_at);
+        $created_at = $date->format('Y-m-d H:i:s');
+
+        $userName = $tweet->user["name"];
+        $userPicture = $tweet->user["profile_image_url"];
+        $text = $tweet->text;
+
+    ?>
+
+
+
+        <div class='card' style='width: 18rem;'>
+            <div class='card-body'>
+                <div class="userPicture"><img src="<?php echo $userPicture; ?>" /></div>
+                <h5 class='card-title'><?php echo $userName; ?></h5>
+                <h6><?php echo $created_at; ?></h6>
+                <p class='card-text'><?php echo $text; ?></p>
+            </div><!-- .card-body -->
+        </div><!-- .card -->
+
+    <?php
+    }
+    ?>
+
+</section>
