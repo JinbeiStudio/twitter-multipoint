29bdd6545f323b3550a31a81d9b5e710c5711a7b
GabrielTavernier <gabrieltavernier@gmail.com>
2021-03-23 14:02:47 +0000

    maj css index

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
 <?php require_once './views/component/head.php';
 require_once './views/component/header.php'; ?>
 
-<form action="./functions/recherche.php" method="post">
-    <div class="input-group">
-        <div class="form-outline">
-            <input name="recherche" type="search" id="recherche" class="form-control" />
-            <label class="form-label" for="form1">Rechercher</label>
+<main class="container mt-4">
+
+
+
+    <form action="./functions/recherche.php" method="post">
+        <div class="form-group">
+            <label for="recherche">Tapez votre recherche</label>
+            <div class="input-group mb-3">
+
+                <input type="text" id="recherche" class="form-control" placeholder="Recherche" aria-label="search">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary">
-            <i class="fas fa-search"></i>
-        </button>
-    </div>
-</form>
+    </form>
 
-<?php
-if (isset($_SESSION['recherche'])) {
-    require_once('./views/template/result.php');
-} else {
-    echo "Pas de recherche effectuée.";
-}
+    <?php
+    if (isset($_SESSION['recherche'])) {
+        require_once('./views/template/result.php');
+    } else {
+        echo "Pas de recherche effectuée.";
+    }
+    ?>
 
-?>
+</main>
+
+
 <?php require_once('./views/component/footer.php'); ?>
