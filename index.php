2a7a132bc7c5aed3c89b4930c6dc887006697a1b
JinbeiStudio <julien.gabriel@me.com>
2021-03-23 13:56:25 +0000

    required search

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once './views/component/header.php'; ?>
 <form action="./functions/recherche.php" method="post">
     <div class="input-group">
         <div class="form-outline">
-            <input name="recherche" type="search" id="recherche" class="form-control" />
+            <input required name="recherche" type="search" id="recherche" class="form-control" />
             <label class="form-label" for="form1">Rechercher</label>
         </div>
         <button type="submit" class="btn btn-primary">
@@ -17,7 +17,7 @@ require_once './views/component/header.php'; ?>
 $view = './views/template/start.php';
 if (isset($_SESSION['recherche'])) {
     $view = './views/template/result.php';
-} 
+}
 require_once $view;
 
 ?>
