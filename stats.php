6f886169f05220bb29aeadbcd2c8a40b445c980a
JinbeiStudio <julien.gabriel@me.com>
2021-03-24 14:29:51 +0000

    page stats

---
+++ stats.php
@@ -0,0 +1,22 @@
+<?php
+require_once './functions/Twitter.php';
+require_once './views/component/head.php';
+require_once './views/component/header.php';
+?>
+
+<main class="container mt-4">
+    <form action="./functions/recherche.php" method="post">
+        <div class="form-group">
+            <label for="recherche">Tapez votre recherche</label>
+            <div class="input-group mb-3">
+
+                <input type="text" id="recherche" name="recherche" class="form-control" placeholder="Recherche" aria-label="search">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+</main>
--- views/component/header.php
+++ views/component/header.php
@@ -9,7 +9,8 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="index.php">Rechercher les derniers tweets<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="stats.php">Statistiques par pays</a>
                     <a class="nav-link" href="./functions/logout.php">Déconnexion</a>
                 </div>
             </div>
