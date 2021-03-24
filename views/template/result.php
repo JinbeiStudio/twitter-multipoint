5a6682c7b44d153271b8e233e70d6f9f9d656a83
GabrielTavernier <gabrieltavernier@gmail.com>
2021-03-24 07:44:13 +0000

    màj result.php pour modifier l'affichage.

--- views/template/result.php
+++ views/template/result.php
@@ -1,14 +1,13 @@
 <!--p>Result</p-->
 
 <pre>
-
 <?php
-// print_r(unserialize($_SESSION['recherche'])); 
+// print_r(unserialize($_SESSION['recherche']));
 ?>
 </pre>
 
 
-<section class="row">
+<section class="d-flex flex-wrap justify-content-center">
 
     <?php
     foreach (unserialize($_SESSION['recherche']) as $tweet) {
@@ -17,14 +16,26 @@
         $created_at = $date->format('Y-m-d H:i:s');
 
         $userName = $tweet->user["name"];
-        $userPicture = $tweet->user["profile_image_url"];
+
+        $userPicture = "./src/img/default_profile_400x400.png";
+        if (!empty($tweet->user["profile_image_url"])) {
+            $userPicture = $tweet->user["profile_image_url"];
+        }
+        if (!empty($tweet->user["profile_banner_url"])) {
+            $userBackground = $tweet->user["profile_banner_url"];
+        }
+
+        $userBackground = $tweet->user["profile_banner_url"];
         $text = $tweet->text;
 
     ?>
 
 
 
-        <div class='card' style='width: 18rem;'>
+        <div class='card m-2' style='width: 18rem;'>
+            <?php if ($userBackground) { ?>
+                <img src="<?php echo $userBackground; ?>" class="card-img-top">
+            <?php } ?>
             <div class='card-body'>
                 <div class="userPicture"><img src="<?php echo $userPicture; ?>" /></div>
                 <h5 class='card-title'><?php echo $userName; ?></h5>
