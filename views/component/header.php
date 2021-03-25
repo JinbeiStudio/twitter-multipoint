349b679a2c2ea75b4105b5439d442f15084cdad6
GabrielTavernier <gabrieltavernier@gmail.com>
2021-03-25 14:03:01 +0000

    Ajout footer et correction ligne résultat

--- views/component/header.php
+++ views/component/header.php
@@ -1,9 +1,9 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">
+            <span class="logo">
                 <img src="../../src/img/logo-twitter.svg" alt="Twitter" width="300" height="244" class="nav-logo" />
                 Twitter project
-            </a>
+            </span>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- views/template/result.php
+++ views/template/result.php
@@ -1,10 +1,5 @@
 <?php $tweets = unserialize($_SESSION['recherche']); ?>
 
-<?php
-//Affichage du nb de twweets trouvés ou message en cas de résultat nul
-echo count($tweets) ? count($tweets) . ' Résultats' :  'Aucun résultat n\'as été trouvé. Elargissez votre recherche.';
-?>
-
 
 <section class="d-flex flex-wrap justify-content-center">
 
